f3d5365623ee7d837aa7db2712312532fd3dd263
Anfu95 <angeelfm18@gmail.com>
2022-06-13 10:55:14 -0500

    query scopes in categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,7 +15,11 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::included()->
+                                filter()->
+                                sort()->
+                                getOrPaginate();
+        return $categories;
     }
 
     /**
@@ -26,7 +30,14 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'slug' => 'required|max:255|unique:categories',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return $category;
     }
 
     /**
@@ -35,9 +46,10 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function show(Category $category)
+    public function show($id)
     {
-        //
+        $category = Category::included()->findOrfail($id);
+        return $category;
     }
 
     /**
@@ -49,7 +61,14 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'slug' => 'required|max:255|unique:categories,slug,'.$category->id,
+        ]);
+
+        $category->update($request->all());
+
+        return $category;
     }
 
     /**
@@ -60,6 +79,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return $category;
     }
 }
